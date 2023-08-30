05b79121a98539c0c2cc7e2eb6061a28b9b83929
Nick <90505328+napswastaken@users.noreply.github.com>
2023-08-31 09:13:02 +1200

    os.makedirs(directory)

--- DayzConfigSearch.py
+++ DayzConfigSearch.py
@@ -1,3 +1,12 @@
+"""
+This Python script is designed scan through config.cpp files and look for classnames, and will dump those classnames to a list. the classnames will only show in the list if they have a
+scope of 2, to avoid _base or _colorbase classnames. Change class_pattern if required.
+"""
+__author__ = "naps"
+__copyright__ = "Copyright (C) 2023 Nick Shepherd"
+__license__ = "General Public License v3.0"
+__version__ = "1.0"
+
 import os
 import re
 
@@ -5,7 +14,7 @@ import re
 root_directory = r"Path\\to\\Folder"
 
 #output where you want the text file to go
-output_file_path = os.path.join(os.path.expanduser("~"), "Downloads/export/classnames.txt")
+output_file_path = os.path.join(os.path.expanduser("~"), "Downloads", "export", "classnames.txt")
 
 # REGEX for class, classname must have scope 2, or it skips it
 class_pattern = r"class\s+(\w+)\s*:\s*\w+\s*\{[^}]*scope\s*=\s*2;\s*"
@@ -21,17 +30,19 @@ def process_config_file(file_path):
     return classnames
 
 def write_classnames_to_file(classnames):
+    directory = os.path.dirname(output_file_path)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
     with open(output_file_path, "w") as output_file:
         for classname in classnames:
             output_file.write(classname + "\n")
-
 def main():
     if not os.path.exists(root_directory):
         print("Directory does not exist.")
         return
-
     classnames = []
-
     for root, _, files in os.walk(root_directory):
         for file in files:
             if file.lower() == "config.cpp":
